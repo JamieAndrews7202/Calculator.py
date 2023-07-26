e1b8d3a218fce418e339f6c51a28ccb989f14010
JamieAndrews7202 <77287572+JamieAndrews7202@users.noreply.github.com>
2023-07-25 23:12:47 -0500

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,1 +1,50 @@
+#def main():
+#   x = int(input("What's x? "))
+#    print("x sqaured is", square(x))
+    
+    
+#def square(n):
+#    return pow(n, 2)
+
+#main()
+
+
+#List of actions
+print("Hello! I am a calcutor. What kind of problem can I help you solve? ")
+print("1: Add")
+print("2: Substract")
+print("3: Multiply")
+print("4: Divide")
+
+#Ask user what them need to do
+
+a = int(input("What kind of problem do you need to solve? Please enter 1-4. "))
+
+# If the user wants to add two numbers
+if a == 1 :
+    x = int(input("What is your first number? "))
+    y = int(input("What is your second number? "))
+    print(x + y)
+
+# If the user wants to subtract two numbers
+elif a == 2 :
+    x = int(input("What is your first number? "))
+    y = int(input("What is your second number? "))
+    print(x - y)
+
+
+# If the user wants you mutiply two numbers
+elif a == 3 :
+    x = int(input("What is your first number? "))
+    y = int(input("What is your second number? "))
+    print(x * y)
+
+# If the user wants to divide two numbers
+elif a == 4 :
+    x = int(input("What is your first number? "))
+    y = int(input("What is your second number? "))
+    print(x / y)
+
+else:
+    print("Not a option. Please choose from 1, 2, 3 or 4. ")
 
